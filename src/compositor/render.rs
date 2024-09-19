0029f3929cd5562a23ea909b0eea6a22e0928cdc
Juanperias <juanperiasyt@gmail.com>
2024-09-19 17:15:52 -0400

    feat: render text job

--- src/compositor/render.rs
+++ src/compositor/render.rs
@@ -1,1 +1,30 @@
+use nannou::prelude::*;
 
+use crate::{context::globals::JOBS, models::job::JobKind};
+
+pub fn render_jobs(app: &App, _model: &(), frame: Frame) {
+    let jobs_guard = JOBS
+        .get()
+        .expect("Error: cannot get the jobs")
+        .lock()
+        .expect("Error: cannot lock the jobs");
+    let jobs = &*jobs_guard;
+
+    let draw = app.draw();
+    draw.background().color(WHITE);
+
+    for job in jobs {
+        let win_rect = app.main_window().rect().pad(20.0);
+
+        match job.kind {
+            JobKind::Text => {
+                draw.text(&job.value)
+                    .color(BLACK)
+                    .font_size(20)
+                    .wh(win_rect.wh());
+            }
+        }
+    }
+
+    draw.to_frame(app, &frame).unwrap();
+}
--- src/compositor/window.rs
+++ src/compositor/window.rs
@@ -3,6 +3,8 @@ use anyhow::Result;
 use nannou::prelude::*;
 use std::sync::{Mutex, OnceLock};
 
+use super::render::render_jobs;
+
 pub static SCREEN_STATE: OnceLock<Mutex<Screen>> = OnceLock::new();
 
 pub fn remember_screen(screen: Screen) {
@@ -25,7 +27,7 @@ pub fn model(app: &App) {
         .lock()
         .expect("Failed to lock the mutex");
 
-    let mut window_builder = app.new_window().view(view);
+    let mut window_builder = app.new_window().view(render_jobs);
 
     if let (Some(size_x), Some(size_y)) = (screen_static.size_x, screen_static.size_y) {
         window_builder = window_builder
@@ -37,24 +39,4 @@ pub fn model(app: &App) {
     let _window_id = window_builder
         .build()
         .expect("Error: cannot build the window");
-}
-
-fn view(app: &App, _model: &(), frame: Frame) {
-    let draw = app.draw();
-    draw.background().color(WHITE);
-
-    let win_rect = app.main_window().rect().pad(20.0);
-
-    let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nResize the window to test dynamic layout.";
-
-    //                         L     o     r     e     m           i    p    s    u    m
-    let glyph_colors = vec![BLUE, BLUE, BLUE, BLUE, BLUE, BLACK, RED, RED, RED, RED, RED];
-
-    draw.text(text)
-        .color(BLACK)
-        .glyph_colors(glyph_colors)
-        .font_size(24)
-        .wh(win_rect.wh());
-
-    draw.to_frame(app, &frame).unwrap();
 }
