c675f711eafb712a8c8add5643e8845f4e1ab6ab
Juanperias <juanperiasyt@gmail.com>
2024-09-23 17:42:28 -0400

    feat: improve config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,16 @@
-pub const FLEXY_CHANNEL: &str = "Unstable";
+pub enum Channel {
+    Unstable,
+    Stable,
+}
+
+impl Channel {
+    pub fn to_str(&self) -> &'static str {
+        match self {
+            Channel::Unstable => "Unstable",
+            Channel::Stable => "Stable",
+        }
+    }
+}
+
+pub const FLEXY_CHANNEL: Channel = Channel::Unstable;
 pub const FLEXY_VERSION: &str = "0.1";
