620b37ff7d53995b4bf7b70f667663612d427f06
Juanperias <juanperiasyt@gmail.com>
2024-09-26 15:54:21 -0400

    refactor: better way to manage the fonts

--- src/widgets/text.rs
+++ src/widgets/text.rs
@@ -3,18 +3,14 @@ use nannou::prelude::*;
 use crate::models::job::Job;
 
 pub fn render(draw: &Draw, win_rect: Rect, job: &Job) {
+    let text = draw
+        .text(&job.value)
+        .color(job.style.color)
+        .font_size(job.style.font_size)
+        .x_y_z(job.style.pos_x, job.style.pos_y, 0.0)
+        .wh(win_rect.wh());
+
     if let Some(font) = &job.style.font {
-        draw.text(&job.value)
-            .font(font.to_owned())
-            .color(job.style.color)
-            .font_size(job.style.font_size)
-            .x_y_z(job.style.pos_x, job.style.pos_y, 0.0)
-            .wh(win_rect.wh());
-    } else {
-        draw.text(&job.value)
-            .color(job.style.color)
-            .font_size(job.style.font_size)
-            .x_y_z(job.style.pos_x, job.style.pos_y, 0.0)
-            .wh(win_rect.wh());
+        text.font(font.to_owned());
     }
 }
