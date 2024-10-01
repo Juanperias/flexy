17250e6d8e746bd905d10ddf20c517a22dd181b0
Juanperias <juanperiasyt@gmail.com>
2024-10-01 15:51:56 -0400

    feat: add basic on_click event

--- src/compositor/render.rs
+++ src/compositor/render.rs
@@ -1,33 +1,34 @@
+use crate::{context::globals::JOBS, models::job::Kind, widgets::text};
 use sdl2::{rect::Rect, render::Canvas, ttf::Sdl2TtfContext, video::Window};
 
-use crate::{context::globals::JOBS, models::job::Kind, widgets::text};
-
-pub fn jobs(canvas: &mut Canvas<Window>, ttf: &Sdl2TtfContext) -> Result<(), String> {
+pub fn jobs(
+    canvas: &mut Canvas<Window>,
+    ttf: &Sdl2TtfContext,
+) -> Result<Vec<(Rect, String)>, String> {
     let jobs_guard = JOBS
         .get()
         .expect("Error: cannot get the jobs")
         .lock()
         .expect("Error: cannot lock the jobs");
     let jobs = &*jobs_guard;
-    let texture_creature = canvas.texture_creator();
+    let texture_creator = canvas.texture_creator();
+    let mut job_rects = Vec::new();
 
     for job in jobs {
         match &job.kind {
             Kind::Text => {
-                let (texture, surface) = text::render(ttf, job, &texture_creature)?;
-                canvas.copy(
-                    &texture,
-                    None,
-                    Some(Rect::new(
-                        job.style.pos_x,
-                        job.style.pos_y,
-                        surface.width(),
-                        surface.height(),
-                    )),
-                )?;
+                let (texture, surface) = text::render(ttf, job, &texture_creator)?;
+                let rect = Rect::new(
+                    job.style.pos_x,
+                    job.style.pos_y,
+                    surface.width(),
+                    surface.height(),
+                );
+                canvas.copy(&texture, None, Some(rect))?;
+                job_rects.push((rect, job.value.clone()));
             }
         }
     }
 
-    Ok(())
+    Ok(job_rects)
 }
--- src/compositor/window.rs
+++ src/compositor/window.rs
@@ -7,14 +7,15 @@ use sdl2::event::{Event, WindowEvent};
 use std::env;
 use std::time::Duration;
 
-use super::render;
+use sdl2::{mouse::MouseButton, rect::Rect};
+
+use super::render::jobs;
 
 pub fn render(screen: &Screen, can_close: bool, lua: &Lua) -> Result<(), String> {
     if env::var("WAYLAND_DISPLAY").is_ok() {
         env::set_var("SDL_VIDEODRIVER", "wayland");
         println!("[{}] video driver set to wayland", "DEBUG".blue());
     }
-
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
     let ttf_context = sdl2::ttf::init().map_err(|err| err.to_string())?;
@@ -29,22 +30,39 @@ pub fn render(screen: &Screen, can_close: bool, lua: &Lua) -> Result<(), String>
         .borderless()
         .build()
         .map_err(|err| err.to_string())?;
-
     let mut canvas = window
         .into_canvas()
         .accelerated()
         .build()
         .map_err(|err| err.to_string())?;
-
     let mut event_pump = sdl_context.event_pump()?;
+    let mut job_rects: Vec<(Rect, String)> = Vec::new();
+
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } => {
                     if can_close {
-                        break 'main;
+                        break 'main Ok(());
                     }
                     println!("[{}] cannot close this window", "ERROR".red());
+                }
+                Event::MouseButtonDown {
+                    x, y, mouse_btn, ..
+                } => {
+                    if mouse_btn == MouseButton::Left {
+                        for (rect, text) in &job_rects {
+                            let expanded_rect = Rect::new(
+                                rect.x() - 10,
+                                rect.y() - 10,
+                                rect.width() + 20,
+                                rect.height() + 20,
+                            );
+                            if expanded_rect.contains_point((x, y)) {
+                                println!("Clicked on text: {}", text);
+                            }
+                        }
+                    }
                 }
                 Event::Window {
                     timestamp: _,
@@ -66,14 +84,12 @@ pub fn render(screen: &Screen, can_close: bool, lua: &Lua) -> Result<(), String>
                 _ => {}
             }
         }
-
         if let Some(bg_color) = &screen.bg_color {
             let color = bg_color.to_color().map_err(|err| err.to_string())?;
             canvas.set_draw_color(color);
         } else {
             canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
         }
-
         canvas.clear();
         let update: Function = match lua.globals().get("Update") {
             Ok(func) => func,
@@ -82,11 +98,8 @@ pub fn render(screen: &Screen, can_close: bool, lua: &Lua) -> Result<(), String>
                 .map_err(|e| e.to_string())?,
         };
         update.call::<(), ()>(()).map_err(|err| err.to_string())?;
-        render::jobs(&mut canvas, &ttf_context)?;
+        job_rects = jobs(&mut canvas, &ttf_context)?;
         canvas.present();
-
         std::thread::sleep(Duration::from_millis(100));
     }
-
-    Ok(())
 }
